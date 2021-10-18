55cdc17c59c5379817bf6ed33b4ebf9be607300b
aditiii710 <65843740+aditiii710@users.noreply.github.com>
2021-10-18 13:48:01 +0530

    Folder structure updated

---
+++ src/routes/App.js
@@ -0,0 +1,19 @@
+// import React, { Component, Suspense } from "react";
+// import { Switch, Route } from "react-router-dom";
+// import routes from "./routes";
+// import Header from "../App/Layouts/Header";
+// import Footer from "../App/Layouts/Footer";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Header />
+//         <Suspense fallback={<div />}>{/* <Switch>{menu}</Switch> */}</Suspense>
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
